42e04a453ccce36b1c6478b03e78a36993d5e099
Anna-Kyra <anna-kyra.strik@hva.nl>
2024-10-19 15:21:32 +0200

    Public src files
    Ik werk nu in de src map daar zitten al mijn files.
    
    Heb nu inplaats van _site (default), de map public gemaakt waar nu de gewone html code word gegenereerd.
    
    De public folder neemt niet alles mee dus die moet je zelf erbij zetten met eleventyConfig.addPassthroughCopy('')
    
    In de eleventy.js file kan je zien dat nu de styles en assets map naar de public map word gegenereerd.
    
    Je moet eerst terminaten en wanneer er iets nieuws in de eleventy.js zit kan je npm run dev doen zodat de veranderingen worden aangemaakt in de public map

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,14 @@
-module.exports = function(eleventyConfig) {
-    // Waar komen de source files vandaan
+module.exports = function(eleventyConfig) { //kan zonder en met function maar met function kan je meer
+    
+    eleventyConfig.addPassthroughCopy('./src/styles')
+    eleventyConfig.addPassthroughCopy('./src/assets')
+    
+    // Waar komen de source files vandaan 
+    // Waar zijn de public facing files located
     return {
         dir: {
-            input: "src", //in de src folder
-            output: "public" //in de public folder
+            input: "src", // Waar ik in werk , nu de src map
+            output: "public" //Default is _site, maar wil hem in de public folder omdat dat logischer is
         }
     }
 }
