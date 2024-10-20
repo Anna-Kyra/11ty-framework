e11018c2238c906d3fdfc527fb4b7c58a7c92bed
Anna-Kyra <anna-kyra.strik@hva.nl>
2024-10-20 16:16:40 +0200

    begin contentful inloaden
    bron: https://www.contentful.com/blog/integrating-contentful-with-eleventy-create-static-sites/
    
    - In _data is contentful.js
    - {{ contentful }} in je html zetten
    - in .env file je api keys zetten
    
    - npm install --save-dev contentful dotenv
    - require('dotenv').config() boven aan je .eleventy.js zetten
    - - dan wordt het toegevoegd aan de progess.env
    
    met
    pagination:
       data: contentful
    
    maakt hij pagina's aan uit contentful in de public map.

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,6 @@
+require('dotenv').config() // API keys in de env worden toegevoegd aan de process.env
+// console.log(process.env)
+
 module.exports = function(eleventyConfig) { //kan zonder en met function maar met function kan je meer
     
     eleventyConfig.addPassthroughCopy('./src/styles')
@@ -8,7 +11,7 @@ module.exports = function(eleventyConfig) { //kan zonder en met function maar me
     return {
         dir: {
             input: "src", // Waar ik in werk , nu de src map
-            output: "public" //Default is _site, maar wil hem in de public folder omdat dat logischer is
+            output: "public" // Default is _site, maar wil hem in de public folder omdat dat logischer is
         }
     }
 }
