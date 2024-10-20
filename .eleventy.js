c064be516712f851e273a17d0a3cdd1eda2f33d5
Anna-Kyra <anna-kyra.strik@hva.nl>
2024-10-20 17:19:29 +0200

    Redirect 1.2
    Bron: https://www.sitebuilderone.com/blog/11ty-url-redirections.html
    
    Method 2: Generating Redirect Files
    
    Zo geprobeert, werkt nogsteeds niet.
    
    proberen of de netflify plugin wel werkt

--- .eleventy.js
+++ .eleventy.js
@@ -5,7 +5,16 @@ const fs = require('fs');
 
 module.exports = function(eleventyConfig) { //kan zonder en met function maar met function kan je meer
     
-    eleventyConfig.addPassthroughCopy('redirect.njk')
+    eleventyConfig.addCollection("redirects", function(collection) {
+        let redirects = JSON.parse(fs.readFileSync("src/_data/redirects.json"));
+        return redirects.map(redirect => ({
+          data: {
+            permalink: redirect.from,
+            layout: "redirect.njk"
+          },
+          redirect: redirect.to
+        }));
+      });
     eleventyConfig.addPassthroughCopy('./src/styles')
     eleventyConfig.addPassthroughCopy('./src/assets')
     
