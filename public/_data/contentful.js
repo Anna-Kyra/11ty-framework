8eab624ce4ca3ec3298767d057c6d329190eac7e
Anna-Kyra <anna-kyra.strik@hva.nl>
2024-10-20 20:57:16 +0200

    Netflify bug 1.0
    Bug:
    Expected parameter accessToken (via TypeError)
    
    Kan accessToken niet vinden
    
    oplossing?
    eleventyConfig.addPassthroughCopy('./src/_data') in eleventy.js?

---
+++ public/_data/contentful.js
@@ -0,0 +1,21 @@
+const contentful = require("contentful") // Wordt de Contentful JavaScript SDK (client library) geimporteert
+const client = contentful.createClient({ // nieuwe client aangemaakt met de tokens van de env file, zorgt ervoor dat het dynamisch word
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+})
+
+// console.log(client)
+
+module.exports = function() {
+    return client.getEntries({ content_type: 'page', order: 'sys.createdAt' })
+    .then(function(response) {
+        const page = response.items
+        .map(function(page) {
+            page.fields.date= new Date(page.sys.updatedAt);
+            return page.fields;
+        });
+        return page;
+    })
+    .catch(console.error);
+};
+
