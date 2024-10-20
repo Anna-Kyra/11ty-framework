e28626ea761ecb60a46f5d926f074e7ce72d873c
Anna-Kyra <anna-kyra.strik@hva.nl>
2024-10-20 17:45:30 +0200

    redirects met plugin 1.3
    Bron: https://www.npmjs.com/package/eleventy-plugin-netlify-redirects

--- .eleventy.js
+++ .eleventy.js
@@ -1,22 +1,15 @@
 require('dotenv').config() // API keys in de env worden toegevoegd aan de process.env
 // console.log(process.env)
-
-const fs = require('fs');
+const EleventyPluginNetlifyRedirects = require("eleventy-plugin-netlify-redirects");
 
 module.exports = function(eleventyConfig) { //kan zonder en met function maar met function kan je meer
     
-    eleventyConfig.addCollection("redirects", function(collection) {
-        let redirects = JSON.parse(fs.readFileSync("src/_data/redirects.json"));
-        return redirects.map(redirect => ({
-          data: {
-            permalink: redirect.from,
-            layout: "redirect.njk"
-          },
-          redirect: redirect.to
-        }));
-      });
     eleventyConfig.addPassthroughCopy('./src/styles')
     eleventyConfig.addPassthroughCopy('./src/assets')
+
+    const eleventyPluginNetlifyRedirectsOptions = {};
+    eleventyConfig.addPlugin(EleventyPluginNetlifyRedirects, eleventyPluginNetlifyRedirectsOptions);
+    
     
     // Waar komen de source files vandaan 
     // Waar zijn de public facing files located
