115b36120899670a94e406670f053f22439e2bf9
Anna-Kyra <anna-kyra.strik@hva.nl>
2024-10-20 20:40:19 +0200

    redirect 1.4
    nog iets geprobeert, werkt nogsteeds niet. Ik kom er later weer op terug

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,12 @@
 require('dotenv').config() // API keys in de env worden toegevoegd aan de process.env
 // console.log(process.env)
-const EleventyPluginNetlifyRedirects = require("eleventy-plugin-netlify-redirects");
+
 
 module.exports = function(eleventyConfig) { //kan zonder en met function maar met function kan je meer
-    
     eleventyConfig.addPassthroughCopy('./src/styles')
     eleventyConfig.addPassthroughCopy('./src/assets')
+    eleventyConfig.addPassthroughCopy('./src/redirect.html')
 
-    const eleventyPluginNetlifyRedirectsOptions = {};
-    eleventyConfig.addPlugin(EleventyPluginNetlifyRedirects, eleventyPluginNetlifyRedirectsOptions);
-    
-    
     // Waar komen de source files vandaan 
     // Waar zijn de public facing files located
     return {
