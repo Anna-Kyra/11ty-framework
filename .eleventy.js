9e4e7f28796a4c124b57b9cae4d8282327202476
Anna-Kyra <anna-kyra.strik@hva.nl>
2024-10-20 21:29:47 +0200

    netlify bug 1.4
    public verwijderd en weer aangemaakt

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,4 @@
-require('dotenv').config() // API keys in de env worden toegevoegd aan de process.env
+require('dotenv').config(); // API keys in de env worden toegevoegd aan de process.env
 // console.log(process.env)
 // console.log('space = ' + process.env.CONTENTFUL_SPACE_ID) werkt
 // console.log('access token = ' + process.env.CONTENTFUL_ACCESS_TOKEN) werkt
@@ -7,7 +7,6 @@ require('dotenv').config() // API keys in de env worden toegevoegd aan de proces
 module.exports = function(eleventyConfig) { //kan zonder en met function maar met function kan je meer
     eleventyConfig.addPassthroughCopy('./src/styles')
     eleventyConfig.addPassthroughCopy('./src/assets')
-    eleventyConfig.addPassthroughCopy('./src/redirect.html')
 
     // Waar komen de source files vandaan 
     // Waar zijn de public facing files located
