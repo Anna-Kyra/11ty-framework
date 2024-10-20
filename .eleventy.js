30c6f53ba7e95b44b3132a075559ea9b040b274a
Anna-Kyra <anna-kyra.strik@hva.nl>
2024-10-20 17:13:31 +0200

    Redirect 1.0
    bron: https://www.sitebuilderone.com/blog/11ty-url-redirections.html
    
    Geprobeert om een redirect te maken van / naar /home
    
    werkt nog niet echt

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,11 @@
 require('dotenv').config() // API keys in de env worden toegevoegd aan de process.env
 // console.log(process.env)
 
+const fs = require('fs');
+
 module.exports = function(eleventyConfig) { //kan zonder en met function maar met function kan je meer
     
+    eleventyConfig.addPassthroughCopy('redirect.njk')
     eleventyConfig.addPassthroughCopy('./src/styles')
     eleventyConfig.addPassthroughCopy('./src/assets')
     
@@ -14,4 +17,6 @@ module.exports = function(eleventyConfig) { //kan zonder en met function maar me
             output: "public" // Default is _site, maar wil hem in de public folder omdat dat logischer is
         }
     }
+
+    
 }
